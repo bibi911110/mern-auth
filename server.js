ef725aeb2c4b15ce6874958d2368b76a0d9aae93
bibo54524 <happidev54524@gmail.com>
2021-09-01 00:55:35 +0800

    user model, auth controller logic

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const express = require('express');
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const connectDB = require("./config/db");
+
+//ConnectDB
+connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+app.use(express.json());
 
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
